c6d55213d4ceb38100f93b455dd08c00f0d4282c
S5 <carlos.diaz@sinco.com.co>
2018-03-20 07:59:48 -0500

    A
    A

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,7 +6,10 @@ const FNB = s5.initialize();
 FNB.require([], () => {
     const FB = s5.get('.facebook').shift();
     const IG = s5.get('.instagram').shift();
+    const menu = s5.get('.icono').shift();
+    const nav = s5.get('.menu > nav').shift();
 
     FB.addEvent('click', () => window.open('https://fb.me/friendsnbikes'));
     IG.addEvent('click', () => window.open('https://www.instagram.com/friendsnbikes'));
+    menu.addEvent('click', () => nav.classList.toggle('visible'));
 });
