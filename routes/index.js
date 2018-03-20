64f85dc8948b07aab804b2ee81ead8fdea0e5005
S5 <carlos.diaz@sinco.com.co>
2018-03-20 09:20:42 -0500

    A
    A

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var anio = new Date().getFullYear();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '@FriendsNbikes - FnB', inicio: true, about: false, events: false });
+  res.render('index', { title: '@FriendsNbikes - FnB', inicio: true, about: false, events: false, anio: anio });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Nosotros - @FriendsNbikes', inicio: false, about: true, events: false });
+  res.render('about', { title: 'Nosotros - @FriendsNbikes', inicio: false, about: true, events: false, anio: anio });
 });
 
 router.get('/events', function(req, res, next) {
-  res.render('events', { title: 'Eventos - @FriendsNbikes', inicio: false, about: false, events: true });
+  res.render('events', { title: 'Eventos - @FriendsNbikes', inicio: false, about: false, events: true, anio: anio });
 });
 
 module.exports = router;
